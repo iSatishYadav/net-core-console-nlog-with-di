0769df832ed6348d924cd95a05dfc3e869f7b52b
Satish Yadav <monstersatish@live.com>
2021-05-22 12:35:42 +0530

    Add project files.

---
+++ SkyLogs/Person.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyLogs
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        private readonly ILogger<Person> _logger;
+
+        public Person(ILogger<Person> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Talk(string text)
+        {
+            _logger.LogInformation("Person {name} spoke {text}", Name, text);
+        }
+    }
+}
---
+++ SkyLogs/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using System;
+
+namespace SkyLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                var config = new ConfigurationBuilder()
+                   .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .Build();
+
+                var servicesProvider = BuildDi(config);
+
+                using (servicesProvider as IDisposable)
+                {
+                    var person = servicesProvider.GetRequiredService<Person>();
+                    person.Name = "Sky";
+                    person.Talk("Hello");
+
+                    Console.WriteLine("Press ANY key to exit");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch any exception and log it.
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+        private static IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+                 //Add DI Classes here
+               .AddTransient<Person>() // Runner is the custom class
+               .AddLogging(loggingBuilder =>
+               {
+                   // configure Logging with NLog
+                   loggingBuilder.ClearProviders();
+                   loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                   loggingBuilder.AddNLog(config);
+               })
+               .BuildServiceProvider();
+        }
+
+    }
+}
